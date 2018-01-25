6929683df5bad79defe8bfcab306bfad66e66be9
Family-TreeSY <286210002@qq.com>
2018-01-25 13:46:15 +0800

    Django project

---
+++ typeidea/typeidea/settings/develop.py
@@ -0,0 +1,16 @@
+# coding:utf-8
+
+from .base import *  # NOQA
+
+'''
+NOQA:告诉PEP8检测工具，这里不需要检测
+'''
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
