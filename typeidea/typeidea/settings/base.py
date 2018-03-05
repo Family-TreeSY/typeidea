34291173ee850a7512f455015285af61b73e7a40
Family-TreeSY <286210002@qq.com>
2018-03-05 19:38:47 +0800

    '增加静态处理'

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Django settings for typeidea project.
 
@@ -126,4 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' # 静态资源路径
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, THEME, 'static'),
+]
