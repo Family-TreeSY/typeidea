dd8706a70bd7fbed2f5ec125165ce2df642001a8
Family-TreeSY <286210002@qq.com>
2018-03-06 17:40:06 +0800

    '增加友链'

--- typeidea/config/views.py
+++ typeidea/config/views.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from blog.views import CommonMixin
+from .models import Link
+
+class LinkView(CommonMixin, ListView):
+    queryset = Link.objects.filter(status=1)
+    model = Link
+    template_name = 'config/links.html'
+    context_object_name = 'links'
--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from .custom_site import custom_site
+from config.views import LinkView
 from blog.views import IndexView, CategoryView, TagView, PostView, AuthorView
 
 '''
@@ -30,6 +31,7 @@ urlpatterns = [
     url(r'^tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag'),
     url(r'^post/(?P<pk>\d+)/$', PostView.as_view(), name='detail'),
     url(r'^author/(?P<author_id>\d+)/$', AuthorView.as_view(), name='author'),
+    url(r'^links/$', LinkView.as_view(), name='links'),
     # url(r'^post/(?P<post_id>\d+).html$', post_detail)
     url(r'^admin/', admin.site.urls),
     url(r'^cus_admin/', custom_site.urls),
